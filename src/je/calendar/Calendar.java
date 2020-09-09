f3745d53f23d9fe28876eaf7707d198a694cb8d5
kui93 <kui93@211.105.89.24>
2020-09-09 14:52:17 +0900

    Add Leapyear funtion

--- src/je/calendar/Calendar.java
+++ src/je/calendar/Calendar.java
@@ -3,23 +3,36 @@ package je.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
-	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month-1];
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
+
+	public int getMaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
+		}
+	}
+
 	public void printCalendar(int year, int month) {
-		System.out.printf("  << %4d년%3d월 >>\n",year,month);
+		System.out.printf("  << %4d년%3d월 >>\n", year, month);
 		System.out.println(" 일 월 화 수 목 금 토");
 		System.out.println("---------------------");
-		
-		int maxDay = getMaxDaysOfMonth(month);
-		
-		for(int i = 1; i <=maxDay; i++) {
-			System.out.printf("%3d",i);
-			if(i %7 == 0) {
+
+		int maxDay = getMaxDaysOfMonth(year, month);
+
+		for (int i = 1; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == 0) {
 				System.out.println();
 			}
 		}
@@ -29,6 +42,5 @@ public class Calendar {
 //		System.out.println("15 16 17 18 19 20 21");
 //		System.out.println("22 23 24 25 26 27 28");
 	}
-	
 
 }
